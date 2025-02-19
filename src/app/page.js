8d4728ea9e7dbb33ae94668b28840621b1d37b42
AlbertAdekanye <adekanyealbert@gmail.com>
2025-02-19 16:33:43 +0100

    #

--- src/app/page.js
+++ src/app/page.js
@@ -1,37 +1,24 @@
-import Results from "@/components/Results";
+import Results from '@/components/Results';
 
 const API_KEY = process.env.API_KEY;
 
 export default async function Home({ searchParams }) {
-  try {
-    const genre = searchParams.genre || "fetchTrending";
-    const endpoint = genre === "fetchTopRated" ? "/movie/top_rated" : "/trending/all/week";
-    
-    const res = await fetch(
-      `https://api.themoviedb.org/3${endpoint}?api_key=${API_KEY}&language=en-US`,
-      { cache: "no-store" },
-      {next: {revalidate: 10000}}
-    );
-  
+  const genre = searchParams.genre || 'fetchTrending';
+  const res = await fetch(
+    `https://api.themoviedb.org/3${
+      genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
+  );
+  const data = await res.json();
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+  const results = data.results;
 
-    if (!res.ok) {
-      throw new Error("Failed to fetch data");
-    }
-
-    const data = await res.json();
-    console.log(data.results);
-
-    const results = data.results;
-
-    return (
-      <div>
+  return (
+    <div>
       <Results results={results} />
     </div>
-    );
-  } catch (error) {
-    console.error(error);
-    return <div>
-      <Results result={result} />
-    </div>;
-  }
+  );
 }
